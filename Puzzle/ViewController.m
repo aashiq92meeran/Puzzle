c8ac3a49458c6cb97445fc1089df082a69e347e7
vishnupriya93 <vishnu.priya@hispark.in>
2014-06-27 11:45:08 +0530

    Added Place holder for the image Drag gesture left
    Commit

--- Puzzle/ViewController.m
+++ Puzzle/ViewController.m
@@ -17,6 +17,7 @@
 @end
 
 @implementation ViewController
+
 @synthesize images;
 
 
@@ -30,16 +31,36 @@
     
 //  UIImage * image = [[UIImage alloc] init];
     
-  UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(50, 70, 670, 400)];
-       view2.backgroundColor = [UIColor blueColor];
+    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(50, 70, 670, 400)];
     
+       view2.backgroundColor = [UIColor blueColor];
+  
+    // Place Holders for images
+    
+    UIImageView *dot1 =[[UIImageView alloc] initWithFrame:CGRectMake(30,50,80,70)];
+    dot1.backgroundColor = [UIColor redColor];
+    
+    UIImageView *dot2 =[[UIImageView alloc] initWithFrame:CGRectMake(110,120,80,70)];
+    dot2.backgroundColor = [UIColor brownColor];
+    UIImageView *dot3 =[[UIImageView alloc] initWithFrame:CGRectMake(110,50,80,70)];
+    dot3.backgroundColor = [UIColor blackColor];
+    UIImageView *dot4 =[[UIImageView alloc] initWithFrame:CGRectMake(30,120,80,70)];
+    dot4.backgroundColor = [UIColor purpleColor];
 
+    
+        [view2 addSubview:dot1];
+        [view2 addSubview:dot2];
+        [view2 addSubview:dot3];
+        [view2 addSubview:dot4];
+    
+    
     int x=10;
     int y=10;
 
     [self.view addSubview:view2];
     
     UIView *view1 = [[UIView alloc]initWithFrame:CGRectMake(50,530,670,400)];
+    
     view1.backgroundColor = [UIColor redColor];
    
     
@@ -51,7 +72,7 @@
             }
             
           [self panImages];
-          UIImageView * dot = [[UIImageView alloc] initWithFrame:CGRectMake(150+x, 170, 50, 50)];
+          UIImageView * dot = [[UIImageView alloc] initWithFrame:CGRectMake(50+x+x, 170, 80, 70)];
             
             NSString * imageName=[NSString stringWithFormat:@"%@",[images objectAtIndex:i]];
     dot.image=[UIImage imageNamed:imageName];
@@ -88,16 +109,24 @@
 {
 
 images = [NSMutableArray arrayWithObjects:
-                         @"1.png",
+                         @"1.png" ,
                          @"2.png",
-                         @"3.png",@"4.png",nil];
+                         @"3.png",
+                         @"4.png",nil];
     
 }
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    
-    
-}
+//     if ([touches count] == 1)
+//     {
+//  CGPoint touchPoint = [[touches anyObject] locationInView:self.view];
+//         for (UIImageView *iView in self.view.subviews) {
+//             if ([iView isMemberOfClass:[UIImageView class]]) {
+//                 if (touchPoint.x > iView.frame.origin.x &&
+//                     touchPoint.x < iView.frame.origin.x + iView.frame.size.width &&
+//                     touchPoint.y > iView.frame.origin.y &&
+//                     touchPoint.y < iView.frame.origin.y + iView.frame.size.height)
+                     }
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
     
